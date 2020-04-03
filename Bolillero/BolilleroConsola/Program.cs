88876f88f186fa0ebadcc1121fdce76cfcef5302
TheDiegoNG <diego20022011@hotmail.com>
2020-04-02 22:10:37 -0300

    BolilleroConsola
    Clases y prueba de Jugada

---
+++ Bolillero/BolilleroConsola/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BolilleroBiblioteca;
+using System.Collections.Generic;
+
+namespace BolilleroConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bolillero bolillero = new Bolillero();
+            for (int i = 0; i < 40; i++)
+            {
+                bolillero.bolillasLista.Add(i);
+            }
+
+            List<int> jugada = bolillero.sacarJugada();
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine(jugada[i]);
+            }
+
+
+        }
+    }
+}
